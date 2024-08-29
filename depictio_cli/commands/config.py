1e9cf12069774f1110796f23d916c1bc5a993c98
Thomas Weber <weber8thomas@gmail.com>
2024-08-29 23:12:56 +0200

    refactor: Update show_config command to accept agent config path as an option

--- depictio_cli/commands/config.py
+++ depictio_cli/commands/config.py
@@ -1,10 +1,13 @@
 from depictio_cli.utils import load_depictio_config
 import typer
-from typing import Optional
+from typing import Annotated, Optional
 
 app = typer.Typer()
 
+
 @app.command()
-def show_config():
-    depictio_agent_config = load_depictio_config("~/.depictio/agent.yaml")
+def show_config(
+    agent_config_path: Annotated[str, typer.Option("--agent-config-path", help="Path to the configuration file")] = "~/.depictio/agent.yaml",
+):
+    depictio_agent_config = load_depictio_config(config_path=agent_config_path)
     typer.echo(depictio_agent_config)
