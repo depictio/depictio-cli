391349a095e81a63b5d858971cbc6bef7bcb5424
Thomas Weber <weber8thomas@gmail.com>
2024-12-13 16:49:29 +0100

    refactor: reorganize CLI structure, remove unused files, and update pipeline configuration

--- depictio_cli/depictio_cli.py
+++ depictio_cli/depictio_cli.py
@@ -1,11 +1,12 @@
 import typer
-
-from depictio_cli.commands.config import app as config
-from depictio_cli.commands.data import app as data
+from typer.main import get_command
+from depictio_cli.cli.commands.config import app as config
+from depictio_cli.cli.commands.data import app as data
 
 app = typer.Typer()
 app.add_typer(config, name="config")
 app.add_typer(data, name="data")
+depictiocli = get_command(app)
 
 
 def main():
