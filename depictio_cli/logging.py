1097e10d5aef190daa4852404a36d1c710324163
Thomas Weber <weber8thomas@gmail.com>
2025-01-24 16:18:52 +0100

    feat: refactor logging setup to allow configurable verbosity and improved formatting

--- depictio_cli/logging.py
+++ depictio_cli/logging.py
@@ -1,29 +1,36 @@
+# depictio_cli/logging.py
 import logging
 from colorlog import ColoredFormatter
 
-# Create a logger
+# Initialize logger without handlers
 logger = logging.getLogger("depictio-cli")
-logger.setLevel(logging.DEBUG)
+logger.propagate = False  # Prevent propagation to root logger
 
-# Create a colored formatter
-formatter = ColoredFormatter(
-    "%(log_color)s%(asctime)s%(reset)s | %(cyan)s%(name)s%(reset)s | %(green)s%(levelname)s%(reset)s | %(yellow)s%(funcName)s:%(lineno)d%(reset)s | %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
-        'DEBUG':    'cyan',
-        'INFO':     'green',
-        'WARNING':  'yellow',
-        'ERROR':    'red',
-        'CRITICAL': 'red,bg_white',
-    },
-    secondary_log_colors={},
-    style='%'
-)
 
-# Create a console handler and set the formatter
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
+def setup_logging(verbose: bool = False) -> logging.Logger:
+    global logger
 
-# Add the handler to the logger
-logger.addHandler(handler)
+    # Clear any existing handlers
+    logger.handlers.clear()
+    formatter = ColoredFormatter(
+        "%(log_color)s%(asctime)s%(reset)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(filename)s - %(funcName)s - line %(lineno)d - %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    if verbose:
+        logger.setLevel(logging.INFO)
+    else:
+        logger.setLevel(logging.ERROR)
+
+    return logger
